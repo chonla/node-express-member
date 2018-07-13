3a9df329d9d5e28fb7a038842e60acb7430a9952
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-13 14:40:25 +0700

    pack middleware into array

--- routes.js
+++ routes.js
@@ -17,10 +17,10 @@ module.exports = (app) => {
     router.patch('/me', [cors(), security.secure], UserService.updateMe)
     router.get('/me', [cors(), security.secure], UserService.getMe)
 
-    router.get('/bnk/members', cors(),  MemberService.getMember)
-    router.post('/bnk/members', cors(), security.secure, MemberService.createMember)
-    router.patch('/bnk/members/:id', cors(), security.secure, MemberService.updateMember)
-    router.delete('/bnk/members/:id', cors(), security.secure, MemberService.deleteMember)
+    router.get('/bnk/members', cors(), MemberService.getMember)
+    router.post('/bnk/members', [cors(), security.secure], MemberService.createMember)
+    router.patch('/bnk/members/:id', [cors(), security.secure], MemberService.updateMember)
+    router.delete('/bnk/members/:id', [cors(), security.secure], MemberService.deleteMember)
 
     app.use(router)
     app.use((err, req, res, next) => {
