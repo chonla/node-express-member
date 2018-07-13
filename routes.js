d76083b24b64158c5e5d22151ab3011b63cbc21d
Witaya Tospitakkul <witaya1412@gmail.com>
2018-07-13 11:30:41 +0700

    remove jwt checker for get route

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
     router.patch('/me', [cors(), security.secure], UserService.updateMe)
     router.get('/me', [cors(), security.secure], UserService.getMe)
 
-    router.get('/bnk/members', cors(), security.secure, MemberService.getMember)
+    router.get('/bnk/members', cors(),  MemberService.getMember)
     router.post('/bnk/members', cors(), security.secure, MemberService.createMember)
     router.patch('/bnk/members/:id', cors(), security.secure, MemberService.updateMember)
     router.delete('/bnk/members/:id', cors(), security.secure, MemberService.deleteMember)
